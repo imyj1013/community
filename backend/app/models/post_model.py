3f52e9888e807a8c6652d40caa93bc5a4a6d89f9
imyj1013 <imyj1013@gmail.com>
2025-11-23 23:59:18 +0900

    add text summarization

--- backend/app/models/post_model.py
+++ backend/app/models/post_model.py
@@ -1,6 +1,6 @@
 from app import db
 
-def create_post(user_id, title, content, image_url, nickname, time):
+def create_post(user_id, title, content, summary, image_url, nickname, time):
     post_id = db.counters["post"]
     db.counters["post"] += 1
 
@@ -9,6 +9,7 @@ def create_post(user_id, title, content, image_url, nickname, time):
         "user_id": user_id,
         "title": title,
         "content": content,
+        "summary": summary,
         "image_url": image_url,
         "author_nickname": nickname,
         "created_at": time,
@@ -24,9 +25,10 @@ def create_post(user_id, title, content, image_url, nickname, time):
 def get_post_by_id(post_id: int):
     return next((p for p in db.posts_db if p["post_id"] == post_id), None)
 
-def update_post(post, title, content, image_url, time):
+def update_post(post, title, content, summary, image_url, time):
     post["title"] = title
     post["content"] = content
+    post["summary"] = summary
     post["image_url"] = image_url
     post["updated_at"] = time
     return post
